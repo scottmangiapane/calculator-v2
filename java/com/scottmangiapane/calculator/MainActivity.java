2cccd6988559a8205848534cf5a638f09ca82352
Scott Mangiapane <scottmangiapane@gmail.com>
2016-06-18 17:44:40 -0700

    Toolbar and menu button
    Added a clear actionbar (toolbar) to the top of the screen with an
    overflow icon.

--- java/com/scottmangiapane/calculator/MainActivity.java
+++ java/com/scottmangiapane/calculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.scottmangiapane.calculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private CalculatorView calculatorView;
@@ -11,9 +14,27 @@ public class MainActivity extends AppCompatActivity {
         // setTheme(R.style.AppTheme_Dark);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
         calculatorView = new CalculatorView(this);
         if (savedInstanceState != null)
             calculatorView.setText(savedInstanceState.getString("text"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_options) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
