2898886abacbe6ead15a020e7ef23a161eab68dd
Scott Mangiapane <scottmangiapane@gmail.com>
2016-07-26 16:14:50 -0700

    SettingsActivity; Styles
    Using a PreferenceFragment instead of a PreferenceActivity; removed
    ActionBar styles.

--- java/com/scottmangiapane/calculatorv2/SettingsActivity.java
+++ java/com/scottmangiapane/calculatorv2/SettingsActivity.java
@@ -1,12 +1,28 @@
 package com.scottmangiapane.calculatorv2;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        if (sp != null && sp.getBoolean("pref_dark", false))
+            setTheme(R.style.AppTheme_Dark);
+        else
+            setTheme(R.style.AppTheme_Light);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
