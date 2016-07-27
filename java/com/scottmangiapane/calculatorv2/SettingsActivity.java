89a317ed2930e58bbbf05a23d38afac24fa4d501
Scott Mangiapane <scottmangiapane@gmail.com>
2016-07-26 22:30:48 -0700

    Themes
    Finished implementing themes.

--- java/com/scottmangiapane/calculatorv2/SettingsActivity.java
+++ java/com/scottmangiapane/calculatorv2/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.scottmangiapane.calculatorv2;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -15,10 +18,54 @@ public class SettingsActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        if (sp != null && sp.getBoolean("pref_dark", false))
-            setTheme(R.style.AppTheme_Dark);
+        if (sp.getBoolean("pref_dark", false))
+            switch (sp.getString("pref_theme", "Blue")) {
+                case "0":
+                    setTheme(R.style.AppTheme_Dark_Blue);
+                    break;
+                case "1":
+                    setTheme(R.style.AppTheme_Dark_Cyan);
+                    break;
+                case "2":
+                    setTheme(R.style.AppTheme_Dark_Gray);
+                    break;
+                case "3":
+                    setTheme(R.style.AppTheme_Dark_Green);
+                    break;
+                case "4":
+                    setTheme(R.style.AppTheme_Dark_Indigo);
+                    break;
+                case "5":
+                    setTheme(R.style.AppTheme_Dark_Purple);
+                    break;
+                case "6":
+                    setTheme(R.style.AppTheme_Dark_Red);
+                    break;
+            }
         else
-            setTheme(R.style.AppTheme_Light);
+            switch (sp.getString("pref_theme", "Blue")) {
+                case "0":
+                    setTheme(R.style.AppTheme_Light_Blue);
+                    break;
+                case "1":
+                    setTheme(R.style.AppTheme_Light_Cyan);
+                    break;
+                case "2":
+                    setTheme(R.style.AppTheme_Light_Gray);
+                    break;
+                case "3":
+                    setTheme(R.style.AppTheme_Light_Green);
+                    break;
+                case "4":
+                    setTheme(R.style.AppTheme_Light_Indigo);
+                    break;
+                case "5":
+                    setTheme(R.style.AppTheme_Light_Purple);
+                    break;
+                case "6":
+                    setTheme(R.style.AppTheme_Light_Red);
+                    break;
+            }
         setContentView(R.layout.activity_settings);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -30,6 +77,26 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+            findPreference("pref_theme").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    ListPreference lp = (ListPreference) findPreference("pref_theme");
+                    String url = getActivity().getPackageName();
+                    String previous = url + ".MainActivity-" + lp.getEntry();
+                    String next = url + ".MainActivity-" + lp.getEntries()[Integer.parseInt(o.toString())];
+                    getActivity().getPackageManager().setComponentEnabledSetting(
+                            new ComponentName(url, previous),
+                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+                    getActivity().getPackageManager().setComponentEnabledSetting(
+                            new ComponentName(url, next),
+                            PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+                    TaskStackBuilder.create(getActivity())
+                            .addNextIntent(new Intent(getActivity(), MainActivity.class))
+                            .addNextIntent(getActivity().getIntent())
+                            .startActivities();
+                    return true;
+                }
+            });
             findPreference("pref_dark").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object o) {
