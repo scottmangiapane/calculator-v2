267ad1b5960a3add500040fad994060900b549e6
Scott Mangiapane <scottmangiapane@gmail.com>
2016-07-26 17:08:24 -0700

    Refresh settings
    Refreshed settings when dark theme switch is changed.

--- java/com/scottmangiapane/calculatorv2/SettingsActivity.java
+++ java/com/scottmangiapane/calculatorv2/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.scottmangiapane.calculatorv2;
 
-import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,7 +13,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         if (sp != null && sp.getBoolean("pref_dark", false))
             setTheme(R.style.AppTheme_Dark);
         else
@@ -27,6 +29,17 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+            findPreference("pref_dark").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    getActivity().finish();
+                    Intent i = new Intent(getActivity(), SettingsActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                    return true;
+                }
+            });
         }
     }
 }
