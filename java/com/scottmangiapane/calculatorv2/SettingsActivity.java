a46fe35fd7cc5e4fa2ea917b21cd248b96c3d158
Scott Mangiapane <scottmangiapane@gmail.com>
2016-07-26 23:32:28 -0700

    Cleanup
    Cleaned up files.

--- java/com/scottmangiapane/calculatorv2/SettingsActivity.java
+++ java/com/scottmangiapane/calculatorv2/SettingsActivity.java
@@ -1,15 +1,8 @@
 package com.scottmangiapane.calculatorv2;
 
-import android.content.ComponentName;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -70,43 +63,5 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getFragmentManager().beginTransaction().replace(R.id.content, new SettingsFragment()).commit();
-    }
-
-    public static class SettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-            findPreference("pref_theme").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object o) {
-                    ListPreference lp = (ListPreference) findPreference("pref_theme");
-                    String url = getActivity().getPackageName();
-                    String previous = url + ".MainActivity-" + lp.getEntry();
-                    String next = url + ".MainActivity-" + lp.getEntries()[Integer.parseInt(o.toString())];
-                    getActivity().getPackageManager().setComponentEnabledSetting(
-                            new ComponentName(url, previous),
-                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-                    getActivity().getPackageManager().setComponentEnabledSetting(
-                            new ComponentName(url, next),
-                            PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-                    TaskStackBuilder.create(getActivity())
-                            .addNextIntent(new Intent(getActivity(), MainActivity.class))
-                            .addNextIntent(getActivity().getIntent())
-                            .startActivities();
-                    return true;
-                }
-            });
-            findPreference("pref_dark").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object o) {
-                    TaskStackBuilder.create(getActivity())
-                            .addNextIntent(new Intent(getActivity(), MainActivity.class))
-                            .addNextIntent(getActivity().getIntent())
-                            .startActivities();
-                    return true;
-                }
-            });
-        }
     }
 }
